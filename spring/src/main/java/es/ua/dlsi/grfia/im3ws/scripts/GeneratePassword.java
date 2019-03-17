5cb04130b0d89b358f3f04a0ca8b09140fff5ec1
davidrizo <picabia2a>
2019-03-17 11:37:17 +0100

    Changes to make it work in production

--- spring/src/main/java/es/ua/dlsi/grfia/im3ws/scripts/GeneratePassword.java
+++ spring/src/main/java/es/ua/dlsi/grfia/im3ws/scripts/GeneratePassword.java
@@ -2,11 +2,18 @@ package es.ua.dlsi.grfia.im3ws.scripts;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.Scanner;
+
 public class GeneratePassword {
     public static final void main(String [] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        System.out.println("Introduce password:");
+        Scanner scanner = new Scanner(System.in); // don't use Console to make it work inside IntelliJ
+
+        String passwd = scanner.nextLine();
         System.out.println("Encoded password");
-        System.out.println(encoder.encode(args[0]));
+        System.out.println(encoder.encode(passwd));
         System.out.println("----------------");
     }
 }
