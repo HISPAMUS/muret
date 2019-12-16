1302ad6c8f8964cb55d4e05c1b84cbd638bdf224
antoniorv6 <ariosvila@gmail.com>
2019-12-16 18:44:31 +0100

    Issue #89 working in backend

--- spring/src/main/java/es/ua/dlsi/grfia/im3ws/muret/controller/UserManagementController.java
+++ spring/src/main/java/es/ua/dlsi/grfia/im3ws/muret/controller/UserManagementController.java
@@ -1,9 +1,12 @@
 package es.ua.dlsi.grfia.im3ws.muret.controller;
 
 import es.ua.dlsi.grfia.im3ws.controller.StringResponse;
+import es.ua.dlsi.grfia.im3ws.muret.controller.payload.PermissionsForm;
 import es.ua.dlsi.grfia.im3ws.muret.controller.payload.RegisterForm;
 import es.ua.dlsi.grfia.im3ws.muret.controller.payload.ResetPWDForm;
 import es.ua.dlsi.grfia.im3ws.muret.entity.User;
+import es.ua.dlsi.grfia.im3ws.muret.repository.CollectionRepository;
+import es.ua.dlsi.grfia.im3ws.muret.repository.PermissionsRepository;
 import es.ua.dlsi.grfia.im3ws.muret.repository.UserRepository;
 import es.ua.dlsi.grfia.im3ws.muret.exceptions.UserManagerException;
 import es.ua.dlsi.grfia.im3ws.muret.model.UserManagerImpl;
@@ -11,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -21,7 +25,7 @@ public class UserManagementController
     private final UserManagerImpl m_userManager;
 
     @Autowired
-    public UserManagementController(UserRepository repository){ m_userManager = new UserManagerImpl(repository);}
+    public UserManagementController(UserRepository repository, CollectionRepository colRep, PermissionsRepository permissionsrepo){ m_userManager = new UserManagerImpl(repository, colRep, permissionsrepo);}
 
     @PostMapping("/register")
     public User createUser(@Valid @RequestBody RegisterForm c_newUserData) throws UserManagerException
@@ -49,5 +53,21 @@ public class UserManagementController
         //If exception has not reached, it means that everything got alright
 
         return new StringResponse("Password changed successfully");
+    }
+
+    @PostMapping("/grantPermissions")
+    @Transactional
+    public StringResponse grantPermissionsOnCollection(@Valid @RequestBody PermissionsForm c_request)
+    {
+        m_userManager.grantPermissions(c_request.getUserName(), c_request.getCollectionID(), c_request.getTypeOfPermission());
+        return new StringResponse("Permissions granted");
+    }
+
+    @PostMapping("/revokePermissions")
+    @Transactional
+    public StringResponse revokePermissionsOnCollection(@Valid @RequestBody PermissionsForm c_request)
+    {
+        m_userManager.revokePermissions(c_request.getUserName(), c_request.getCollectionID());
+        return new StringResponse("Permissions revoked");
     }
 }
