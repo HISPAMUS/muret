0a43b73d8df74414700f91590b3d231bbbf75738
antoniorv6 <ariosvila@gmail.com>
2020-01-08 10:35:15 +0100

    Issue #95 Automatic document analysis controller set up, now it returns a incorrect stub. However, it will go OK when model is developed

---
+++ spring/src/main/java/es/ua/dlsi/grfia/im3ws/muret/controller/payload/CoordinatesDocAnBounding.java
@@ -0,0 +1,41 @@
+package es.ua.dlsi.grfia.im3ws.muret.controller.payload;
+
+public class CoordinatesDocAnBounding
+{
+    Integer x0;
+    Integer y0;
+    Integer xf;
+    Integer yf;
+
+    public Integer getX0() {
+        return x0;
+    }
+
+    public void setX0(Integer x0) {
+        this.x0 = x0;
+    }
+
+    public Integer getY0() {
+        return y0;
+    }
+
+    public void setY0(Integer y0) {
+        this.y0 = y0;
+    }
+
+    public Integer getXf() {
+        return xf;
+    }
+
+    public void setXf(Integer xf) {
+        this.xf = xf;
+    }
+
+    public Integer getYf() {
+        return yf;
+    }
+
+    public void setYf(Integer yf) {
+        this.yf = yf;
+    }
+}
