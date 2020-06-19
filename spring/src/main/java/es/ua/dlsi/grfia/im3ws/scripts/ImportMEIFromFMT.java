ea077239ace8fea5fb4856079a64f64d861f0063
davidrizo <drizovalero@gmail.com>
2020-06-19 12:56:22 +0200

    Update FMT importer: correct x position, add region offset

--- spring/src/main/java/es/ua/dlsi/grfia/im3ws/scripts/ImportMEIFromFMT.java
+++ spring/src/main/java/es/ua/dlsi/grfia/im3ws/scripts/ImportMEIFromFMT.java
@@ -47,10 +47,8 @@ import java.util.*;
 @Transactional
 public class ImportMEIFromFMT implements CommandLineRunner {
     private static final String GROUNDTRUTH_PATH = "/Users/drizo/GCLOUDUA/HISPAMUS/repositorios/musicatradicional/misiones/hispamus/sibelius_finale/alineados";
-    //private static final String DOCUMENT = "C5";
-    //private static final String DOCUMENT = "C14";
-    //private static final String DOCUMENT = "M16";
-    private static final String DOCUMENT = "M38";
+    private static final String [] DOCUMENTS = {"C5", "C14", "M16", "M38"};
+    // private static final String [] DOCUMENTS = {"C5"};
 
     @Autowired
     RegionRepository regionRepository;
@@ -82,125 +80,125 @@ public class ImportMEIFromFMT implements CommandLineRunner {
         new AuthenticateForScripts(authenticationManager).consoleAuthenticate("davidrizo");
 
         String path = GROUNDTRUTH_PATH;
-        String documentName = DOCUMENT;
-        if (args.length == 2) {
+        if (args.length == 1) {
             path = args[0];
-            documentName = args[1];
         } else if (args.length != 0) {
-            throw new Exception("Use: <MEI folders path> <collection name>");
+            throw new Exception("Use: <MEI folders path>");
         }
 
-        System.out.println("Importing using path '" + path + "'");
-        Optional<Document> document = documentRepository.findByName(documentName);
-        if (!document.isPresent()) {
-            throw new Exception("Cannot find collection");
-        }
-        File collectionFolder = new File(path, documentName);
-        if (!collectionFolder.exists()) {
-            throw new Exception("Cannot find collection folder " + collectionFolder.getAbsolutePath());
-        }
+        StringBuilder filesWithErrors = new StringBuilder();
+        for (String documentName: DOCUMENTS) {
+            System.out.println("Importing using path '" + path + "'");
+            Optional<Document> document = documentRepository.findByName(documentName);
+            if (!document.isPresent()) {
+                throw new Exception("Cannot find collection");
+            }
+            File collectionFolder = new File(path, documentName);
+            if (!collectionFolder.exists()) {
+                throw new Exception("Cannot find collection folder " + collectionFolder.getAbsolutePath());
+            }
 
-        ArrayList<File> meiFiles = new ArrayList<>();
-        FileUtils.readFiles(collectionFolder, meiFiles, "mei");
-        StringBuilder filesWithErrors  = new StringBuilder();
-        for (File file: meiFiles) {
-            try {
-                System.out.println("\n------------------------\nImporting MEI file " + file.getName());
-                // look for the image file
-                String jpg = FileUtils.getFileWithoutPathOrExtension(file) + ".jpg";
-                Optional<Image> image = imageRepository.findByFilename(jpg);
-                if (!image.isPresent()) {
-                    throw new Exception("Cannot find image in MuRET named " + jpg);
-                }
-
-                if (image.get().getDocument().getId() != document.get().getId()) {
-                    throw new Exception("Filename does not belong to the document");
-                }
-
-                if (image.get().getPages().size() != 1) {
-                    throw new Exception("Expected 1 page and found " + image.get().getPages().size());
-                }
-
-                Page page = image.get().getPages().get(0);
-
-                // import MEI file
-                MEISongImporter meiSongImporter = new MEISongImporter();
-                ScoreSong scoreSong = meiSongImporter.importSong(file);
-                if (scoreSong.getParts().size() != 1) {
-                    throw new Exception("Expected 1 part and found " + scoreSong.getParts().size());
-                }
-                ScorePart part = scoreSong.getParts().get(0);
-                PageSystemBeginnings pageSystemBeginnings = part.getPageSystemBeginnings();
-
-                if (pageSystemBeginnings == null) {
-                    throw new Exception("Cannot find page system beginnings");
-                }
-
-                // check the number of staves in MuRET matches the number of staves in MEI file
-                TreeMap<Time, SystemBeginning> pageSystemBeginningsMap = pageSystemBeginnings.getSystemBeginnings();
-                List<Region> staves = page.getSortedStaves();
-                if (staves.size() != pageSystemBeginningsMap.size()) {
-                    throw new Exception("MuRET has " + staves.size() + "staves  and MEI file has " + pageSystemBeginnings.getSystemBeginnings().size());
-                }
-
-                // encode agnostic and semantic
-                Encoder encoder = new Encoder(true);
-                encoder.encode(scoreSong);
-                AgnosticEncoding agnosticEncoding = encoder.getAgnosticEncoding();
-                SemanticEncoding semanticEncoding = encoder.getSemanticEncoding();
-
-                // split the encoding into systems
-                List<Sequence<AgnosticToken>> agnosticSystems = new LinkedList<>();
-                Sequence<AgnosticToken> agnosticTokenSequence = null;
-                for (AgnosticToken agnosticToken : agnosticEncoding.getSymbolsWithoutSeparators()) {
-                    if (agnosticToken instanceof AgnosticSystemBreak) {
-                        agnosticTokenSequence = new Sequence<>(); // new sequence
-                        agnosticSystems.add(agnosticTokenSequence);
-                    } else {
-                        if (agnosticTokenSequence == null) {
-                            throw new Exception("Expected a page beginning in MEI that generates an agnostic system beginning");
-                        }
-                        agnosticTokenSequence.add(agnosticToken);
+            ArrayList<File> meiFiles = new ArrayList<>();
+            FileUtils.readFiles(collectionFolder, meiFiles, "mei");
+            for (File file : meiFiles) {
+                try {
+                    System.out.println("\n------------------------\nImporting MEI file " + file.getName());
+                    // look for the image file
+                    String jpg = FileUtils.getFileWithoutPathOrExtension(file) + ".jpg";
+                    Optional<Image> image = imageRepository.findByFilename(jpg);
+                    if (!image.isPresent()) {
+                        throw new Exception("Cannot find image in MuRET named " + jpg);
                     }
-                }
 
-                if (agnosticSystems.size() != staves.size()) {
-                    throw new Exception("The number of agnostic systems (" + agnosticSystems.size() + ") != MuRET staves size (" + staves.size() + ")");
-                }
-
-                List<SemanticEncoding> semanticSystems = new LinkedList<>();
-                SemanticEncoding semanticTokenSequence = null;
-                for (SemanticSymbol semanticSymbol : semanticEncoding.getSymbols()) {
-                    if (semanticSymbol instanceof SemanticSystemBreak) {
-                        semanticTokenSequence = new SemanticEncoding();
-                        semanticSystems.add(semanticTokenSequence);
-                    } else {
-                        if (semanticTokenSequence == null) {
-                            throw new Exception("Expected a page beginning in MEI that generates an semantic system beginning");
-                        }
-
-                        semanticTokenSequence.add(semanticSymbol);
+                    if (image.get().getDocument().getId() != document.get().getId()) {
+                        throw new Exception("Filename does not belong to the document");
                     }
-                }
 
-                // remove last one if empty
-                if (semanticTokenSequence.size() == 0) {
-                    semanticSystems.remove(semanticSystems.size() - 1);
-                }
+                    if (image.get().getPages().size() != 1) {
+                        throw new Exception("Expected 1 page and found " + image.get().getPages().size());
+                    }
 
-                if (semanticSystems.size() != staves.size()) {
-                    throw new Exception("The number of sematic systems (" + agnosticSystems.size() + ") != MuRET staves size (" + staves.size() + ")");
-                }
+                    Page page = image.get().getPages().get(0);
 
-                for (int i = 0; i < staves.size(); i++) {
-                    importStaffContent(i, staves.get(i), agnosticSystems.get(i), semanticSystems.get(i));
+                    // import MEI file
+                    MEISongImporter meiSongImporter = new MEISongImporter();
+                    ScoreSong scoreSong = meiSongImporter.importSong(file);
+                    if (scoreSong.getParts().size() != 1) {
+                        throw new Exception("Expected 1 part and found " + scoreSong.getParts().size());
+                    }
+                    ScorePart part = scoreSong.getParts().get(0);
+                    PageSystemBeginnings pageSystemBeginnings = part.getPageSystemBeginnings();
+
+                    if (pageSystemBeginnings == null) {
+                        throw new Exception("Cannot find page system beginnings");
+                    }
+
+                    // check the number of staves in MuRET matches the number of staves in MEI file
+                    TreeMap<Time, SystemBeginning> pageSystemBeginningsMap = pageSystemBeginnings.getSystemBeginnings();
+                    List<Region> staves = page.getSortedStaves();
+                    if (staves.size() != pageSystemBeginningsMap.size()) {
+                        throw new Exception("MuRET has " + staves.size() + "staves  and MEI file has " + pageSystemBeginnings.getSystemBeginnings().size());
+                    }
+
+                    // encode agnostic and semantic
+                    Encoder encoder = new Encoder(true);
+                    encoder.encode(scoreSong);
+                    AgnosticEncoding agnosticEncoding = encoder.getAgnosticEncoding();
+                    SemanticEncoding semanticEncoding = encoder.getSemanticEncoding();
+
+                    // split the encoding into systems
+                    List<Sequence<AgnosticToken>> agnosticSystems = new LinkedList<>();
+                    Sequence<AgnosticToken> agnosticTokenSequence = null;
+                    for (AgnosticToken agnosticToken : agnosticEncoding.getSymbolsWithoutSeparators()) {
+                        if (agnosticToken instanceof AgnosticSystemBreak) {
+                            agnosticTokenSequence = new Sequence<>(); // new sequence
+                            agnosticSystems.add(agnosticTokenSequence);
+                        } else {
+                            if (agnosticTokenSequence == null) {
+                                throw new Exception("Expected a page beginning in MEI that generates an agnostic system beginning");
+                            }
+                            agnosticTokenSequence.add(agnosticToken);
+                        }
+                    }
+
+                    if (agnosticSystems.size() != staves.size()) {
+                        throw new Exception("The number of agnostic systems (" + agnosticSystems.size() + ") != MuRET staves size (" + staves.size() + ")");
+                    }
+
+                    List<SemanticEncoding> semanticSystems = new LinkedList<>();
+                    SemanticEncoding semanticTokenSequence = null;
+                    for (SemanticSymbol semanticSymbol : semanticEncoding.getSymbols()) {
+                        if (semanticSymbol instanceof SemanticSystemBreak) {
+                            semanticTokenSequence = new SemanticEncoding();
+                            semanticSystems.add(semanticTokenSequence);
+                        } else {
+                            if (semanticTokenSequence == null) {
+                                throw new Exception("Expected a page beginning in MEI that generates an semantic system beginning");
+                            }
+
+                            semanticTokenSequence.add(semanticSymbol);
+                        }
+                    }
+
+                    // remove last one if empty
+                    if (semanticTokenSequence.size() == 0) {
+                        semanticSystems.remove(semanticSystems.size() - 1);
+                    }
+
+                    if (semanticSystems.size() != staves.size()) {
+                        throw new Exception("The number of sematic systems (" + agnosticSystems.size() + ") != MuRET staves size (" + staves.size() + ")");
+                    }
+
+                    for (int i = 0; i < staves.size(); i++) {
+                        importStaffContent(i, staves.get(i), agnosticSystems.get(i), semanticSystems.get(i));
+                    }
+                } catch (Exception e) {
+                    filesWithErrors.append(file.getName());
+                    filesWithErrors.append(':');
+                    filesWithErrors.append(e.toString());
+                    filesWithErrors.append('\n');
+                    e.printStackTrace();
                 }
-            } catch (Exception e) {
-                filesWithErrors.append(file.getName());
-                filesWithErrors.append(':');
-                filesWithErrors.append(e.toString());
-                filesWithErrors.append('\n');
-                e.printStackTrace();
             }
         }
 
@@ -223,8 +221,8 @@ public class ImportMEIFromFMT implements CommandLineRunner {
 
         // now insert all values
         // we consider each agnostic symbol takes the same space
-        int symbolWidth = region.getBoundingBox().getWidth() / agnosticTokenSequence.size();
-        int currentApproxX = 0;
+        int symbolWidth = region.getBoundingBox().getWidth() / (agnosticTokenSequence.size() + 1);
+        int currentApproxX = region.getBoundingBox().getFromX() + symbolWidth;
         List<Symbol> agnosticSymbols = new LinkedList<>();
         for (int i=0; i < agnosticTokenSequence.getSymbols().size(); i++) {
             AgnosticToken token = agnosticTokenSequence.getSymbols().get(i);
