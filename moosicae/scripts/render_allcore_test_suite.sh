e55aab7ee3f29ccf0014349ba8fccf19c5777376
davidrizo <drizovalero@gmail.com>
2020-08-28 18:21:01 +0200

    Merge it with documentation

--- moosicae/scripts/render_allcore_test_suite.sh
+++ moosicae/scripts/render_allcore_test_suite.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #It renders the different encodings and logs the errors to a file
-
+DOCUMENTATIONFOLDER="/Users/drizo/cmg/investigacion/software/github/repositorioHispamus/documentation/mOOsicae/examples/allcoretests"
 INPUTFOLDER="/tmp/allcoretests"
 VEROVIO="verovio --adjust-page-height --adjust-page-width"
 VEROVIO_OUTPUT=" -o "
@@ -11,23 +11,24 @@ LILYPOND_OUTPUT=" -o "
 MUSESCORE="/Applications/MuseScore 3.app/Contents/MacOS/mscore"
 MUSESCORE_OUTPUT=" -o "
 
-ERRORS="/tmp/allcoretests/output/errors"
+
+
+rm -rf ${DOCUMENTATIONFOLDER}
+mkdir -p ${DOCUMENTATIONFOLDER}
+cp -r ${INPUTFOLDER}/* ${DOCUMENTATIONFOLDER}
 
 function processEncoding() {
   extension=$1
   command=$2
   outputSuffix=$3
   outputExtension=$4
-  outputFolder="/tmp/allcoretests/output/${extension}"
 
-  mkdir -p "${ERRORS}/${extension}"
-  mkdir -p ${outputFolder}
-
-  for sourceFile in $(find $INPUTFOLDER -name "*.${extension}" -print); do
-	  echo "Processing ${sourceFile}"
+  for sourceFile in $(find $DOCUMENTATIONFOLDER -name "*.${extension}" -print); do
+	  echo "Processing ${sourceFile}"	  
     filename=$(basename $sourceFile ".${extension}")
-    errorfile="${ERRORS}/${extension}/${filename}.txt"
-    "${command}" ${outputSuffix} "${outputFolder}/${filename}.${outputExtension}" ${sourceFile} 2> ${errorfile}
+    errorfile="${sourceFile}.error.txt"
+    outputfile="${sourceFile}.${outputExtension}"
+    "${command}" ${outputSuffix} ${outputfile} ${sourceFile} 2> ${errorfile}
     if [ ! -s "${errorfile}" ]; then
       rm ${errorfile}
     fi
@@ -36,7 +37,7 @@ function processEncoding() {
 
 
 processEncoding "musicxml" "${MUSESCORE}" "${MUSESCORE_OUTPUT}" "pdf"
-#processEncoding "mei" "${VEROVIO}" "${VEROVIO_OUTPUT}" "svg"
-#processEncoding "skm" "${VEROVIO_SKM}" "${VEROVIO_SKM_OUTPUT}" "svg"
-#processEncoding "ly" "${LILYPOND}" "${LILYPOND_OUTPUT}" ""
+processEncoding "mei" "${VEROVIO}" "${VEROVIO_OUTPUT}" "svg"
+processEncoding "skm" "${VEROVIO_SKM}" "${VEROVIO_SKM_OUTPUT}" "svg"
+processEncoding "ly" "${LILYPOND}" "${LILYPOND_OUTPUT}" ""
 
