d987d9be9f5a095269a545f7e7036c60c6a4d848
davidrizo <drizovalero@gmail.com>
2021-01-31 18:38:34 +0100

    Update layout

--- angular/src/app/layout/components/hamburguer-menu/hamburguer-menu.component.ts
+++ angular/src/app/layout/components/hamburguer-menu/hamburguer-menu.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hamburguer-menu.component.css']
 })
 export class HamburguerMenuComponent implements OnInit {
+  isMenuCollapsed = true;
 
   constructor() { }
 
