e14263c79357ac080ff8de1be59ee83c23018217
davidrizo <drizovalero@gmail.com>
2021-01-31 18:24:15 +0100

    Update angular

--- angular/src/main.ts
+++ angular/src/main.ts
@@ -4,7 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import 'hammerjs';
+// 20210131 import 'hammerjs';
 
 if (environment.production) {
   enableProdMode();
