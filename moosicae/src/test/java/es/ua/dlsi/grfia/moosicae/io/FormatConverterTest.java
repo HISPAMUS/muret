ca3b42072fd3b6a72990785325b58c307f6585e8
davidrizo <drizovalero@gmail.com>
2021-05-23 14:12:35 +0200

    More tests added

--- moosicae/src/test/java/es/ua/dlsi/grfia/moosicae/io/FormatConverterTest.java
+++ moosicae/src/test/java/es/ua/dlsi/grfia/moosicae/io/FormatConverterTest.java
@@ -31,7 +31,13 @@ public class FormatConverterTest {
 
     @Test
     public void convert() throws IMException, IOException {
-        String [] filenames = new String[] {"210097473-1_11_1", "200043752-1_43_2", "000124645-10_1_1", "000122362-1_1_1", "230006508-1_9_1", "000103737-1_1_1"};
+        String [] filenames = new String[] {
+                "201005180-1_11_2", // several nested beams, natural accidental
+                "201009317-1_18_2", // unrequired meter change, multirest in the middle
+                "000103729-1_1_1", // this one is imported from mensural into modern and it has weird bar lines
+                "220017450-1_11_1", // this one contains longa durations in modern notation
+                "000102289-4_1_1", // this one includes two adjacent clef changes at the beginning
+                "000107035-1_20_1", "210097473-1_11_1", "200043752-1_43_2", "000124645-10_1_1", "000122362-1_1_1", "230006508-1_9_1", "000103737-1_1_1"};
         for (String filename: filenames) {
             test(filename);
         }
